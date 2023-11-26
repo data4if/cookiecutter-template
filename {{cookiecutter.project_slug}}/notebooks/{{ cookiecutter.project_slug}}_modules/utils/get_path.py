782ec2d2c3736afc868aea9e8c10456632f48856
carlos4if <carloslopezdata4if@gmail.com>
2023-11-26 16:14:26 -0400

    additiional functions

--- {{cookiecutter.project_slug}}/notebooks/{{ cookiecutter.project_slug}}_modules/utils/get_path.py
+++ {{cookiecutter.project_slug}}/notebooks/{{ cookiecutter.project_slug}}_modules/utils/get_path.py
@@ -1,5 +1,20 @@
 import os
 import pathlib
+import pyhere
+import pyprojroot
+
+
+def make_dir_function(dir_name):
+    def dir_function(*args):
+        return pyprojroot.here().joinpath(dir_name, *args)
+    return dir_function
+
+def get_data_path():
+    # Te regresa dos pasos atras
+    return pyhere.here().resolve() / "data" / "raw"
+
+def get_raw_path():
+    return pyhere.here().resolve() / "data"
 
 def make_path(*args):
     #current_dir=os.getcwd()
