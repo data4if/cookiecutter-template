239b5bff78fd866d2345e76502bf76bf65d3eecd
carlos4if <carloslopezdata4if@gmail.com>
2023-11-26 15:26:26 -0400

    using pathlib to enhance stolen functions

--- {{cookiecutter.project_slug}}/notebooks/{{ cookiecutter.project_slug}}_modules/utils/get_path.py
+++ {{cookiecutter.project_slug}}/notebooks/{{ cookiecutter.project_slug}}_modules/utils/get_path.py
@@ -1,15 +1,21 @@
 import os
+import pathlib
 
 def make_path(*args):
-    current_dir=os.getcwd()
-    new_dir= os.path.join(current_dir,os.pardir,*args)
-    return new_dir
+    #current_dir=os.getcwd()
+    #new_dir= os.path.join(current_dir,os.pardir,*args)
+    CURRENT_DIR = pathlib.Path().resolve()
+    DATA_DIR=CURRENT_DIR.parent.joinpath(args)
+    return DATA_DIR
 
-def make_item_list(*args):
-    data_dir = make_path(*args)
-    items = [os.path.join(data_dir, item) for item in os.listdir(data_dir)]
-    items_list = list(filter(lambda x: os.path.isfile(x), items))
-    return items_list
+def make_path_list(*args):
+    #data_dir = make_path(*args)
+    #items = [os.path.join(data_dir, item) for item in os.listdir(data_dir)]
+    #items_list = list(filter(lambda x: os.path.isfile(x), items))
+    CURRENT_DIR = pathlib.Path().resolve()
+    DATA_DIR=CURRENT_DIR.parent.joinpath(args)
+    # Glob aplica la li
+    return list(DATA_DIR.glob("*"))
 
 
 def make_directories_list(*args):
