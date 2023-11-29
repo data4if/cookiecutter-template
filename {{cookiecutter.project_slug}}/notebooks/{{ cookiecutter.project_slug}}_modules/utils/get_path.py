41f01dfe942b4bde68a1c710c40cc3bfd5375e2d
carlos4if <carloslopezdata4if@gmail.com>
2023-11-29 18:02:01 -0400

    erasing some functions, that was be copy before

--- {{cookiecutter.project_slug}}/notebooks/{{ cookiecutter.project_slug}}_modules/utils/get_path.py
+++ {{cookiecutter.project_slug}}/notebooks/{{ cookiecutter.project_slug}}_modules/utils/get_path.py
@@ -3,38 +3,20 @@ import pathlib
 import pyhere
 import pyprojroot
 
+# Retorna un objeto de tipo directorio
+#def make_dir_function(dir_name):
+#    def dir_function(*args):
+#        return pyprojroot.here().joinpath(dir_name, *args)
+#    return dir_function
 
-def make_dir_function(dir_name):
-    def dir_function(*args):
-        return pyprojroot.here().joinpath(dir_name, *args)
-    return dir_function
-
-def get_data_path(*args):
-    # Te regresa dos pasos atras
-    return pyhere.here().resolve() / args
-
-def get_raw_path():
-    return pyhere.here().resolve() / "data"
-
-def make_path(*args):
-    #current_dir=os.getcwd()
-    #new_dir= os.path.join(current_dir,os.pardir,*args)
+# Retorno un objeto de tipo directorio / de la carpeta padre, hasta el proyecto
+def get_parent_path():
     CURRENT_DIR = pathlib.Path().resolve()
-    DATA_DIR=CURRENT_DIR.parent.joinpath(args)
+    DATA_DIR=CURRENT_DIR.parent
     return DATA_DIR
 
-def make_item_list(*args):
-    #data_dir = make_path(*args)
-    #items = [os.path.join(data_dir, item) for item in os.listdir(data_dir)]
-    #items_list = list(filter(lambda x: os.path.isfile(x), items))
+# Retorna la lista de los path a los diferentes elementos de la carpeta ROOT
+def make_dir_list():
     CURRENT_DIR = pathlib.Path().resolve()
-    DATA_DIR=CURRENT_DIR.parent.joinpath(args)
-    # Glob aplica la li
+    DATA_DIR=CURRENT_DIR.parent
     return list(DATA_DIR.glob("*"))
-
-
-def make_directories_list(*args):
-    data_dir = make_path(*args)
-    dir = [os.path.join(data_dir, item) for item in os.listdir(data_dir)]
-    dir_list = list(filter(lambda x: os.path.isdir(x), dir))
-    return dir_list
